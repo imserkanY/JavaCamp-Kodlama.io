b2a03d11ab0c0c43ea62ad8f6e335dcbd327af3b
imserkanY <serkanyazli@gmail.com>
2021-05-10 16:44:49 +0300

    backend and database

---
+++ lecture-6_hrms/hrms/src/main/java/kodlama/io/hrms/business/concretes/JobPositionManager.java
@@ -0,0 +1,27 @@
+package kodlama.io.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.hrms.business.abstracts.JobPositionService;
+import kodlama.io.hrms.dataAccess.abstracts.JobPositionDao;
+import kodlama.io.hrms.entities.concretes.JobPosition;
+
+@Service
+public class JobPositionManager implements JobPositionService{
+	private JobPositionDao jobPositionDao;
+	
+	@Autowired
+	public JobPositionManager(JobPositionDao jobPositionDao) {
+		super();
+		this.jobPositionDao = jobPositionDao;
+	}
+
+	@Override
+	public List<JobPosition> getAll() {
+		return this.jobPositionDao.findAll();
+	}
+
+}
